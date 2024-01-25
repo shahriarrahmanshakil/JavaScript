8e3deab991ac1f8c2bfa6c73b94df48e0884ae05
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-01-25 11:19:53 +0600

    Home Work Updated

---
+++ Home Work/Statement 25-01-24/switch.js
@@ -0,0 +1,29 @@
+var weekDay = "Friday";
+switch(weekDay) {
+    case 'Saturday':
+        console.log("Today is off day");
+    break;
+
+    case 'Sunday':
+        console.log("Today is Working day");
+    break;
+
+    case 'Monday':
+        console.log("Today is Working day");
+    break;
+
+    case 'Tuesday':
+        console.log("Today is Working day");
+    break;
+
+    case 'Wednsday':
+        console.log("Today is Working day");
+        break;
+
+    case 'Thursday':
+        console.log("Today is Working day");
+    break;
+
+default:
+        console.log("Everyday is beautifull");
+}
