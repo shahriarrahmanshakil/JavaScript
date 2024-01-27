9bbad079432bd9bf2cf2f7d6d05a292fc7ae1d2a
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-01-27 11:38:31 +0600

    Updated

---
+++ Home Work/New folder/demo.js
@@ -0,0 +1,10 @@
+console.log('Hello Bangladesh!');
+var country = 'Bangladesh'
+console.log('Hello' + country + '!');
+var myName = "Shahriar Rahman Shakil"
+console.log(myName);
+console.log('Bangladesh is a Beautifull Country');
+var text ="Varriable is very important for javascript"
+console.log(text);
+
+
---
+++ Home Work/function/function.js
@@ -0,0 +1,46 @@
+console.log("Hello World")
+function fName(){
+    console.log('Shahriar Rahman Shakil');
+}
+fName();
+
+function run(Name) { 
+      console.log(Name  + " is running ");  
+}
+run("Raju");
+run("Rakib");
+run("Raihan");
+
+
+function vos(a,b){
+        return a + b ;
+}
+console.log(vos(8,9));
+console.log(vos(60,90));
+
+function country() {
+    console.log("Shakil");
+    console.log("Management");
+    console.log("Master's of Business Studies");
+    console.log("Dhaka College,Dhaka");
+    console.log("---------------------");
+}
+
+country();
+country();
+country();
+country();
+country();
+
+var y = "goo"
+function bariable() {
+    let x = 6 + 7 ;
+    console.log(x);
+    y =" you"
+    console.log(y);
+}
+
+bariable();
+console.log(y);
+
+
