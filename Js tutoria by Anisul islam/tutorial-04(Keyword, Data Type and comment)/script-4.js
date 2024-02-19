43f067402ef4dd6566225e6c06ab32aa93fa6291
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-02-19 13:10:06 +0600

    Updated

---
+++ Js tutoria by Anisul islam/tutorial-04(Keyword, Data Type and comment)/script-4.js
@@ -0,0 +1,12 @@
+document.write("Shahriar Rahman Shakil<br/>");
+document.write("01933232120<br/>");
+// document.write("Dhaka "); // this is a single line comment 
+document.write("Bangladesh");
+
+/* document.write("Bangladesh");
+ document.write("Bangladesh"); // this is a multi-line comment 
+ document.write("Bangladesh"); 
+ */
+
+
+
