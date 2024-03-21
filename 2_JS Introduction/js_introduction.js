95fe94d9cdd45168fa8fe5c9315e49dcd4048482
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-03-22 03:27:35 +0600

    js introduction file is practice and upload

---
+++ 2_JS Introduction/js_introduction.js
@@ -0,0 +1,24 @@
+function htmlchange(){
+    document.getElementById("Change").innerHTML="<h2 style='color:red;'>JS Can Change HTML Content<h2>";
+}
+
+function onlight(){
+    document.getElementById("bulb").src="onbullb.jpg";
+} 
+
+function offlight(){
+    document.getElementById("bulb").src="offbullb.png";
+}
+
+function changecss(){
+    document.getElementById("changecss").style.color="red";
+}
+
+function displayhide(){
+    document.getElementById("display").style.display="none";
+}
+
+
+function displayshow(){
+    document.getElementById("display").style.display="block";
+}
