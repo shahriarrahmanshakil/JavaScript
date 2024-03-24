f96326fd4a025db9e145e4ce60b08b41a663f8b6
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-03-24 16:48:35 +0600

    javascript output is practicing

---
+++ 4_JS Output/js_output.js
@@ -0,0 +1,9 @@
+//To access an HTML element, JavaScript can use the document.getElementById(id) method.
+//The id attribute defines the HTML element. The innerHTML property defines the HTML content:
+function Changehtml(){
+    document.getElementById("demo").innerHTML="<h2>Changing the innerHTML property of an HTML element is a common way to display data in HTML</h2>";
+}
+
+function mainhtml(){
+    document.getElementById("demo").innerHTML="To access an HTML element, JavaScript can use the document.getElementById(id) method.";
+}
