8dced47ccccea0ba3fe4e2ab2ad277ee415eeb85
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-05-05 18:59:40 +0600

    play audio added

---
+++ Learn-JS-From-Anisul_Islam/tutorial-44(how to play audio in javascript)/playaudio.js
@@ -0,0 +1,24 @@
+for(let i = 0; i < 3; i++){ 
+    document.querySelectorAll(".mybtn")[i].addEventListener("click", function(){
+        var text = this.innerHTML;
+        console.log(text);
+
+
+        switch(text){
+            case"a":
+                var audio = new Audio("arcade.mp3");
+                audio.play();
+                break;
+
+            case"b":
+            var audio = new Audio("retro.mp3");
+            audio.play();
+            break;
+
+            case"c":
+            var audio = new Audio("game-level.mp3");
+            audio.play();
+            break;
+        }
+    })
+}
