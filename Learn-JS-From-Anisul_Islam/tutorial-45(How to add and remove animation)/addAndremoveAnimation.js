931bc675e0c88ab97b9f5e67f928655786f249cb
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-05-08 17:14:11 +0600

    addandremove animation init

--- Learn-JS-From-Anisul_Islam/tutorial-45(How to add and remove animation)/addAndremoveAnimation.js
+++ Learn-JS-From-Anisul_Islam/tutorial-45(How to add and remove animation)/addAndremoveAnimation.js
@@ -1,1 +1,42 @@
-document.querySelectorAll(".")
+for(let i = 0; i < 3; i++)
+    {
+        document.querySelectorAll(".mybtn")[i].addEventListener("click", function(){
+            var text = this.innerHTML;
+            console.log(text);
+            audioPlay(text);
+            adanimation(text);
+        });
+    }
+
+   
+
+    function audioPlay(text)
+    {
+        switch(text){
+            case"a":
+                var audio = new Audio("arcade.mp3");
+                audio.play();
+                break;
+
+            case"b":
+                var audio = new Audio("retro.mp3");
+                audio.play();
+                break;
+
+            case"c":
+                var audio = new Audio("game-level.mp3");
+                audio.play();
+                break;
+        }
+    }
+
+    function adanimation(text)
+    {
+        var dotbtn = document.querySelector("." + text);
+        dotbtn.classList.add("addAnimation");
+
+        setTimeout(function(){
+            dotbtn.classList.remove("addAnimation");
+        },1000);
+    }
+   
