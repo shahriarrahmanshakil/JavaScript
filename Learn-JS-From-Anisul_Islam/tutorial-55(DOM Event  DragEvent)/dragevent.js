317a53a53fb8ed300cc0598a525dc688c0394875
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-05-17 07:15:50 +0600

    dom-drag-event added

---
+++ Learn-JS-From-Anisul_Islam/tutorial-55(DOM Event  DragEvent)/dragevent.js
@@ -0,0 +1,28 @@
+const div = document.querySelector("div");
+const p = document.querySelector("p");
+
+p.addEventListener('dragstart',function(e){
+    e.dataTransfer.setData("Text", e.target.id);
+});
+
+div.addEventListener('dragover', function(e){
+    e.preventDefault();
+});
+
+div.addEventListener('drop', function(e){
+    let id = e.dataTransfer.getData("Text");
+    div.appendChild(document.querySelector("#drag"));
+    e.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+
