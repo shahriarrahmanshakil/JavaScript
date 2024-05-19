829101ccbf3c405179f18cf0657e33eca1f851bb
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-05-19 09:42:43 +0600

    es-6 hosting and strict mode added

---
+++ Learn-JS-From-Anisul_Islam/tutorial-65(hoisting and strict mode)/hostingandstrikmode.js
@@ -0,0 +1,17 @@
+//Hosting and strict mode
+'use strict'
+// x = 20;
+// if(true){
+//     y=30;
+//     console.log(`y = ${y}`);
+// }
+// console.log(`x = ${x}`);
+// var x;
+
+x = 20;
+if(true){
+    y=30;
+    console.log(`y = ${y}`);
+}
+console.log(`x = ${x}`);
+var x;
