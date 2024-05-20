de44bfc8646419bdd0d242cd094562b1dffa0a84
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-05-20 13:15:04 +0600

    array and object destructuring added

---
+++ Learn-JS-From-Anisul_Islam/tutorial-74(Destructuring array and objects)/destructring_Array_and_Object.js
@@ -0,0 +1,14 @@
+const numbers = [10,20,30,40,50];
+let [num1,num2,...num3] = numbers;  //array Destrucring
+console.log(num3);
+
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+
+
+///////////////////////////////////////////////////////
+
+//swap variables
+
