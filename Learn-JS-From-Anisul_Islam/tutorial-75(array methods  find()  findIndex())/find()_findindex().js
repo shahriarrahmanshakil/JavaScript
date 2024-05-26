b42b01aa8ce0e8e877a052d7dadb84fa723a2d36
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-05-26 16:36:44 +0600

    ES-6 find() and findindex() Upgraded

--- Learn-JS-From-Anisul_Islam/tutorial-75(array methods  find()  findIndex())/find()_findindex().js
+++ Learn-JS-From-Anisul_Islam/tutorial-75(array methods  find()  findIndex())/find()_findindex().js
@@ -2,3 +2,32 @@
 let numbers = [5,55,14,5,78];
 let firstEventNumber = numbers.find(x => x % 2 === 0);
 console.log(firstEventNumber);
+
+// findindex()
+let mynumbers = [5,55,14,5,78];
+let FirstEventNumberIndex = numbers.findIndex(x => x % 2 === 0);
+console.log(FirstEventNumberIndex);
+
+/////////////////////////////////////////////////////////
+
+const studentinfo = [
+    {
+        id : 101,
+        name : "Shahriar",
+        CGPA : 3.50
+    },
+    
+    {
+        id : 102,
+        name : "Rahman",
+        CGPA : 4.00
+    },
+
+    {
+        id : 103,
+        name : "Shakil",
+        CGPA : 2.00
+    }
+]
+
+console.log(studentinfo.find(x => x.CGPA > 3));
