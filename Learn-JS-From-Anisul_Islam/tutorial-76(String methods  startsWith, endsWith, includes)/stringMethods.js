e3f096159e6461ad120651a493f7af9f1e012eba
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-05-27 11:21:54 +0600

    ES - 06 string methods added

---
+++ Learn-JS-From-Anisul_Islam/tutorial-76(String methods  startsWith, endsWith, includes)/stringMethods.js
@@ -0,0 +1,11 @@
+const message = "I Love Bangladesh, Bangladesh is Beautiful country";
+
+// startwith(searching, positioin) ....>> 
+console.log(message.startsWith("I",0));
+
+//endwith(searching, length).......>>
+console.log(message.endsWith("Country"));
+console.log(message.endsWith("country"));
+
+//includes(searching, position)
+console.log(message.includes("is"));
