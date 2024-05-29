8fba713d6a37f1a377da8b2110a321fe9036278d
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-05-29 12:42:06 +0600

    problem and solution (even and odd ,divisor added)

---
+++ Problem and Solution With JS/1_Even and Odd Numbers/evenNumber/evenNumber.js
@@ -0,0 +1,8 @@
+let t = parseInt(prompt("Enter You'r Number"));
+for(let i =1; i <= t; i++ ){
+    if(i % 2 == 0){
+        console.log('Even Number<br>');
+    }else{
+        console.log("Not Even Number");
+    }
+}
