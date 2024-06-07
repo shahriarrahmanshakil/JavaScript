6fdf50fa26fd5f34b653494dc66039713f699c5f
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-07 12:02:02 +0600

    dynamic-card project added

---
+++ Learn-JS-From-Anisul_Islam/tutorial-91(mini project 2  how to create dynamic cards)/dynamic_card.js
@@ -0,0 +1,22 @@
+const posts_data = [
+    {
+        title :"title-01",
+        body : "body-01"
+    },
+    {
+        title :"title-02",
+        body : "body-03"
+    },
+    {
+        title :"title-03",
+        body : "body-03"
+    },
+    {
+        title :"title-04",
+        body : "body-04"
+    },
+    {
+        title :"title-05",
+        body : "body-05"
+    }
+]
