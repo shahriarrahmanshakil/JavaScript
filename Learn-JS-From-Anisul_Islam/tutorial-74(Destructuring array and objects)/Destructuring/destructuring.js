2fd996abddb2f811c0031287c7a4c52e47818f8f
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-13 12:27:50 +0600

    swap variables added

--- Learn-JS-From-Anisul_Islam/tutorial-74(Destructuring array and objects)/Destructuring/destructuring.js
+++ Learn-JS-From-Anisul_Islam/tutorial-74(Destructuring array and objects)/Destructuring/destructuring.js
@@ -10,3 +10,17 @@ console.log(n1);
 console.log(n2);
 console.log(...n3); //provides 2 values as string
 console.log(n3);  //provides values as Array 
+
+//swap variables
+let name = "Shahriar Rahman Shakil";
+let number = 1997;
+
+[name,number]=[number,name];
+console.log(number);//provide name variable value
+console.log(name); //provide number variable value 
+
+let a = 100;
+let b = 99;
+[a,b] = [b,a];
+console.log(a); //shows variable b value
+console.log(b); //shows variable a value
