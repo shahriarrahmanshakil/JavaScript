2298bdcdf386aa06a927a3d3fdd0c2634c56c7bf
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-14 23:42:04 +0600

    array filter  method initialize

---
+++ Learn-JS-From-Anisul_Islam/tutorial-71(map and filter array function)/map and filter_2/filter.js
@@ -0,0 +1,6 @@
+//filter method 
+
+const numbers = [29,56,76,44,33,56,89,23,78];
+const newNumbers = numbers.filter((n) => n < 50 );  //useing arrow function 
+console.log(newNumbers);
+console.log(numbers);
