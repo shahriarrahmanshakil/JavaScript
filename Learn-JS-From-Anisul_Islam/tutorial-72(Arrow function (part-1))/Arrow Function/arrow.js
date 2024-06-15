ac9e59ba8b5a9821641605bca4327fa1879ecf15
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-15 23:44:33 +0600

    Arrow Function added

---
+++ Learn-JS-From-Anisul_Islam/tutorial-72(Arrow function (part-1))/Arrow Function/arrow.js
@@ -0,0 +1,29 @@
+// console.log("Arrow Function");
+
+
+//traditional function 
+function walking(){
+    return "I am Walking";
+}
+console.log(walking());
+
+
+//Arrow function 
+const working = () =>{
+    return "I am Working";
+}
+console.log(working());
+
+const working2 = () =>"I am Working-02";
+console.log(working2());
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//How to pass parameter in arrow and traditional function 
+
+
+
+
+
+
