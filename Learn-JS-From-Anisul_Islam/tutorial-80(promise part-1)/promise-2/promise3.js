fcfb8e53f675f5f96b0519f8ec9b7dd4e9aec4da
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-16 20:09:07 +0600

    promise added

--- Learn-JS-From-Anisul_Islam/tutorial-80(promise part-1)/promise-2/promise3.js
+++ Learn-JS-From-Anisul_Islam/tutorial-80(promise part-1)/promise-2/promise3.js
@@ -1,15 +1,29 @@
 //How to Create promise 
 const promise1 = new Promise((resolve,reject)=>{
-        let complitePromise = false;
+        let complitePromise = true;
 
         if(complitePromise){
             resolve("Successfully Complited");
         }else{
-            reject("Unsuccessfull");
+            reject(new Error("Not Complited"));
         }
 });
 promise1.then((res) =>{
     console.log(res);
 }).catch((err) => {
-    console.log(err)
+    console.log(err.message)
 });
+
+///////////////////////////////////////////////////////
+
+const promise2 = new Promise((resolve,reject) =>{
+    resolve("Complited promise-02");
+});
+promise2.then((res2) =>{
+    console.log(res2);
+});
+
+///////////////////////////////////////////////////////////
+Promise.all([promise1,promise2]).then(([res1,res2]) => {
+    console.log(res1,res2);
+})
