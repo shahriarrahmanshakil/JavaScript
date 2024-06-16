19eacbe43ddb5d7462b1132462248b52f6ff57cf
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-17 00:59:43 +0600

    Array Methods added

---
+++ 1_JS Basics/27_JS Array Methods/arrMethod.js
@@ -0,0 +1,5 @@
+
+const cars = ['walton','pegasus','runner','jamuna'];
+document.getElementById('car').innerHTML = cars.length;
+document.getElementById('car1').innerHTML = cars.toString();
+document.getElementById('car2').innerHTML = cars.at(3); 
