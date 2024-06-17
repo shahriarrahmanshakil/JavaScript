5c9e517845c4582d7cc48ffce31adc693148f590
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-17 07:05:53 +0600

    Array more Methods added

--- 1_JS Basics/27_JS Array Methods/arrMethod.js
+++ 1_JS Basics/27_JS Array Methods/arrMethod.js
@@ -3,3 +3,20 @@ const cars = ['walton','pegasus','runner','jamuna'];
 document.getElementById('car').innerHTML = cars.length;
 document.getElementById('car1').innerHTML = cars.toString();
 document.getElementById('car2').innerHTML = cars.at(3); 
+document.getElementById('car3').innerHTML = cars.join("---");
+
+let pop = cars.pop();
+console.log(pop);
+console.log(cars);
+
+let push = cars.push("Jamuna");
+console.log(cars);
+console.log(push);
+
+let shift = cars.shift();
+console.log(cars);
+console.log(shift);
+
+let unshift = cars.unshift('Honda');
+console.log(cars);
+console.log(unshift);
