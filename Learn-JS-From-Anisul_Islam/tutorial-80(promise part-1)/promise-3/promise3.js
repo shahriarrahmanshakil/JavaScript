b159373aafb44c4d76a1625cd713941985cfa8c8
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-05 18:48:49 +0600

    promise all function added

--- Learn-JS-From-Anisul_Islam/tutorial-80(promise part-1)/promise-3/promise3.js
+++ Learn-JS-From-Anisul_Islam/tutorial-80(promise part-1)/promise-3/promise3.js
@@ -1,18 +1,30 @@
 console.log("well-come");
     //How to Create Promise (Pending, Reslove, Reject)
-    const mypromise = new Promise((resolve,reject) => {
-        let complitedPromise = false;
+    const mypromise1 = new Promise((resolve,reject) => {
+        let complitedPromise = true;
         if(complitedPromise){
-            resolve("Promise Successfully Complited");
+            resolve("Promise Successfully Complited - 01");
         }else{
-            reject("Promise Unsuccessfull");
+            reject(new Error("Promise Unsuccessfull"));
         }
     });
 
-    mypromise.then((result) => {
+    mypromise1.then((result) => {
         console.log(result);
     }).catch((error) => {
-        console.log(error)
+        console.log(error.message)
+    });
+
+    //Create Another Promise
+    const mypromise2 = new Promise((resolve,reject) => {
+        resolve("Promise Successfully Complited -02");
+    });
+    mypromise2.then((result) => {
+        console.log(result);
+    })
+
+    Promise.all([mypromise1,mypromise2]).then((result) => {
+        console.log(result);
     });
 
 
