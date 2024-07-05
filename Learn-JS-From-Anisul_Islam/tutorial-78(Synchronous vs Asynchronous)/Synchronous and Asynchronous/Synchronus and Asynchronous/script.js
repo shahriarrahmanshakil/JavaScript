6f29f3c6149b860ca981d0ecf2095ffdbe03c67f
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-05 10:55:24 +0600

    Practice Synchronous and Asynchronous Programming

---
+++ Learn-JS-From-Anisul_Islam/tutorial-78(Synchronous vs Asynchronous)/Synchronous and Asynchronous/Synchronus and Asynchronous/script.js
@@ -0,0 +1,47 @@
+//JavaScript By Default Synchronous Programmig Language
+//Synchronous Programming 
+
+/*
+console.log("Task-01");
+console.log("Task-02");
+console.log("Task-03");
+console.log("Task-04");
+console.log("Task-05");
+console.log("Task-06");
+*/
+
+const Dataloading = () => {
+    console.log("This is Task Four and \nData is Loading");
+}
+
+const taskOne = () => {
+    console.log("This is Task One");
+}
+
+const taskTwo = () => {
+    console.log("This is Task Two");
+}
+
+const taskThree = () => {
+    console.log("This is Task Three");
+}
+
+const taskFour = () => {
+    setTimeout(Dataloading,2000)
+}
+
+const taskFive = () => {
+    console.log("This is Task Five");
+}
+
+const taskSix = () => {
+    console.log("This is Task Six");
+}
+
+taskOne();
+taskTwo();
+taskThree();
+taskFour();
+taskFive();
+taskSix();
+
