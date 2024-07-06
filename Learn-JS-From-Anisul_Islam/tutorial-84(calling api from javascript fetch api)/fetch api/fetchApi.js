07c79056223b99d6b26a5a5adcdd5e0cb28dc5c6
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-06 09:58:47 +0600

    Fetch Api initialize

---
+++ Learn-JS-From-Anisul_Islam/tutorial-84(calling api from javascript fetch api)/fetch api/fetchApi.js
@@ -0,0 +1,11 @@
+/* 
+=> 4 ways to call api (i) XMLHttpRequest (ii) fetch (iii) axios (iv) jQuery
+
+=> fetch() has replaced XMLHttpRequest
+=> fetch() --global method for making HTTP Request
+=> 2 ways to call --then, async await
+=> + fetch() is easy to use compare to XMLHttpRequest
+=> + fetch() returns a promise
+=> - returned promise can only handle network error
+=> - does not support all the older browser
+*/
