08feb0a5cdb91bc13a772efd8d0f72a5a7c80d4b
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-07-06 17:51:29 +0600

    axios api create data  method added

--- Learn-JS-From-Anisul_Islam/tutorial-85(calling api from javascript  axios api)/Axios Api-1/axiosApi.js
+++ Learn-JS-From-Anisul_Islam/tutorial-85(calling api from javascript  axios api)/Axios Api-1/axiosApi.js
@@ -5,12 +5,60 @@
 // .catch((error) => console.log(error))
 
 
-axios.post("https://jsonplaceholder.typicode.com/posts",{
+// axios.post("https://jsonplaceholder.typicode.com/posts",{
+//     method: 'POST',
+//     body: JSON.stringify({
+//       title: 'foo',
+//       body: 'bar',
+//       userId: 1,
+//     })
+// })
+// .then((response) => console.log(response.data))
+
+// axios.put("https://jsonplaceholder.typicode.com/posts/1",{
+//     method: 'PUT',
+//     body: JSON.stringify({
+//         id  : 1,
+//       title : 'Raihan Raju',
+//       body  : 'bar',
+//       userId: 1,
+//     })
+// })
+// .then((response) => console.log(response))
+
+const makeRequest = async (config) => {
+    return await axios(config); 
+}
+
+// const getData = () => {
+//     makeRequest("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => console.log(response))
+//     .catch((error) => console.log(error))
+// }
+// getData();
+
+// const sendData = () => {
+//     makeRequest('https://jsonplaceholder.typicode.com/posts')
+//     .then((response) => console.log(response))
+//     .catch((error) => console.log(error))
+// }
+// sendData();
+
+const createData = () => {
+    makeRequest({
+    url:"https://jsonplaceholder.typicode.com/posts",
     method: 'POST',
     body: JSON.stringify({
-      title: 'foo',
-      body: 'bar',
-      userId: 1,
+    title: 'foo',
+    body: 'bar',
+    userId: 1,}),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
     })
-})
-.then((response) => console.log(response.data))
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+}
+createData();
+
+
