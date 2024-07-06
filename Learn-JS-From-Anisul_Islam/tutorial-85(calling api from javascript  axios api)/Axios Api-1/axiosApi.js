58780c0062797c130d5fd49a846f827a5cd0303f
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-07-06 16:34:34 +0600

    axios api post method added

--- Learn-JS-From-Anisul_Islam/tutorial-85(calling api from javascript  axios api)/Axios Api-1/axiosApi.js
+++ Learn-JS-From-Anisul_Islam/tutorial-85(calling api from javascript  axios api)/Axios Api-1/axiosApi.js
@@ -1,5 +1,16 @@
-console.log(window);
+// console.log(window);
 
-axios.get("https://jsonplaceholder.typicode.com/posts/1")
-.then((response) => console.log(response))
-.catch((error) => console.log(error))
+// axios.get("https://jsonplaceholder.typicode.com/posts/1")
+// .then((response) => console.log(response))
+// .catch((error) => console.log(error))
+
+
+axios.post("https://jsonplaceholder.typicode.com/posts",{
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    })
+})
+.then((response) => console.log(response.data))
