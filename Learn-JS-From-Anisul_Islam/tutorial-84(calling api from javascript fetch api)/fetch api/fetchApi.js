c59aae690eed1847e6bb8a59d68fa786d8995768
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-06 10:27:37 +0600

    fetch api init

--- Learn-JS-From-Anisul_Islam/tutorial-84(calling api from javascript fetch api)/fetch api/fetchApi.js
+++ Learn-JS-From-Anisul_Islam/tutorial-84(calling api from javascript fetch api)/fetch api/fetchApi.js
@@ -9,3 +9,26 @@
 => - returned promise can only handle network error
 => - does not support all the older browser
 */
+
+console.log(window);
+fetch('https://jsonplaceholder.typicode.com/posts/1',{
+    method : 'PUT',
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    body : JSON.stringify({
+        id : 1,
+        title: 'Shahriar Rahman Shakil',
+        body: 'Dhaka,Bangladesh',
+        userId: 1,
+    }),
+})
+.then((response) =>{
+    if(!response.ok){
+        throw new Error("fetch Error")
+    }
+    return response.json();
+})
+.then((response) => console.log(response))
+.catch((error) => console.log(error));
+
